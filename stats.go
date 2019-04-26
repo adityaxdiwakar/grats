677607ffa44a75b1ed22ca11a73110d7c8aa0624
Aditya Diwakar <aditya@diwakar.io>
2019-04-25 22:39:19 -0400

    Some changes, I forgot what, check the diff

--- stats.go
+++ stats.go
@@ -6,11 +6,12 @@ import (
 
 func GenerateDatapoints(messages []Message) []int {
 	var TimeCorrelationPoints []int
+	TimeGrouping := 3600 * 24
 	Counter := -1
 	TimeHolding := float64(-1)
 	for i := 0; i < len(messages); i++ {
 		CreationTimestamp := messages[len(messages)-1-i].CreatedAt
-		CreationTimeHour := math.Floor(float64(CreationTimestamp / (3600)))
+		CreationTimeHour := math.Floor(float64(CreationTimestamp / TimeGrouping))
 		if TimeHolding != -1 && CreationTimeHour != TimeHolding {
 			for {
 				if TimeHolding+1 != CreationTimeHour {
@@ -26,5 +27,6 @@ func GenerateDatapoints(messages []Message) []int {
 		Counter++
 		TimeHolding = CreationTimeHour
 	}
+	TimeCorrelationPoints = append(TimeCorrelationPoints, Counter)
 	return TimeCorrelationPoints
 }
